6b967cc8a97b58c302d1a6ebd8c0b432deeb1db0
Lily Skye <me@suchipi.com>
2024-09-15 12:35:44 -0600

    wait these should be <> not []

--- src/help.ts
+++ src/help.ts
@@ -1,16 +1,16 @@
 export const helpText = `Usage: macaroni [options] <files...>
 
 Options:
-  --include-paths [path1,path2,...]
+  --include-paths <path1,path2,...>
     Comma-separated list of search paths for the builtin #INCLUDE rule;
     folders which contain the filenames you pass to #INCLUDE(...).
 
-  --max-iterations [number]
+  --max-iterations <number>
     Maximum number of times to process macros. When the maximum number of
     iterations is reached, macro expansion is aborted and the process exits
     with a nonzero exit status. The default value is 10.
 
-  --rules [path1,path2,...]
+  --rules <path1,path2,...>
     Comma-separated list of macro rules to load (JavaScript files which export
     functions). By default, only the builtin #INCLUDE rule is used.
     Note that when specifying custom rules, the #INCLUDE rule will not be
